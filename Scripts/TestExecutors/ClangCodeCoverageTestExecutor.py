eb99dfa404359e0c11462bb02a91a994321425f9
David Brownell <db@DavidBrownell.com>
2019-12-30 15:09:30 -0800

    Updated formatting

--- Scripts/TestExecutors/ClangCodeCoverageTestExecutor.py
+++ Scripts/TestExecutors/ClangCodeCoverageTestExecutor.py
@@ -26,7 +26,7 @@ from CppClangCommon.CodeCoverageExecutor import CodeCoverageExecutor as ClangCod
 # ----------------------------------------------------------------------
 _script_fullpath                            = CommonEnvironment.ThisFullpath()
 _script_dir, _script_name                   = os.path.split(_script_fullpath)
-#  ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 # ----------------------------------------------------------------------
 @Interface.staticderived
